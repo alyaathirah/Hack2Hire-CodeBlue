6b28b7cc8e8c89208f17f0969224c7815f5c0e87
Humaira' AS <whomyras00@gmail.com>
2022-11-08 09:49:52 +0800

    sidenav and topnav

--- dce-system/resources/views/auth/login.blade.php
+++ dce-system/resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container position-sticky z-index-sticky top-0">
+    <div class="position-sticky z-index-sticky top-0">
         <div class="row">
             <div class="col-12">
                 @include('layouts.navbars.guest.navbar')
